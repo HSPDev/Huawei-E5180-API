bcc2da7886279b415e17ae89025292d30c31b768
Henrik Sylvester Pedersen <hsp@hsp.dk>
2019-10-24 12:08:57 +0200

    Apply fixes from StyleCI (#9)

--- demo.php
+++ demo.php
@@ -1,13 +1,14 @@
 <?php
+
 require_once 'vendor/autoload.php';
 
 //The router class is the main entry point for interaction.
-$router = new HSPDev\HuaweiApi\Router;
+$router = new HSPDev\HuaweiApi\Router();
 
 //If specified without http or https, assumes http://
 $router->setAddress('192.168.8.1');
 
-//Username and password. 
+//Username and password.
 //Username is always admin as far as I can tell.
 $router->login('admin', 'your-password');
 
